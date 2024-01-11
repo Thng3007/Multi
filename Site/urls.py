844a56c283ea5579c4967a73a5544f3645d66ac8
Thanh Nguyen CTU <thng1701@gmail.com>
2024-01-11 17:08:18 +0700

    Upload Third

--- Site/urls.py
+++ Site/urls.py
@@ -3,5 +3,13 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about'),
+    path('course/', views.course, name='course'),
+    path('research/', views.research, name='research'),
+    path('docs/', views.docs, name='docs'),
+    path('giaotrinh/',views.giaotrinh, name='giaotrinh'),
+    path('lecturer/', views.lecturer, name='lecturer'),
+    
 ]
--- Site/views.py
+++ Site/views.py
@@ -5,3 +5,35 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, "index.html", {})
+
+###
+    # path('contact/', views.contact, name='contact'),
+    # path('about/', views.about, name='about'),
+    # path('course/', views.course, name='course'),
+    # path('research/', views.research, name='research'),
+    # path('docs/', views.docs, name='docs'),
+    # path('giaotrinh/',views.giaotrinh, name='giaotrinh'),
+    # path('lecturer/', views.lecturer, name='lecturer'),
+    
+###
+
+def contact(request):
+    return render(request, "contact.html", {})
+
+def about(request):
+    return render(request, "about.html", {})
+
+def course(request):
+    return render(request, "course.html", {})
+
+def docs(request):
+    return render(request, "docs.html", {})
+
+def giaotrinh(request):
+    return render(request, "giaotrinh.html", {})
+
+def lecturer(request):
+    return render(request, "lecturer.html", {})
+
+def research(request):
+    return render(request, "research.html", {})
