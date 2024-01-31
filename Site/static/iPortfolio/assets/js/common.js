26f08038d8653586fad37bb5d1fe3756eef9c1dc
Thanh Nguyen CTU <thng1701@gmail.com>
2024-01-31 15:08:39 +0700

    Config language

--- Site/static/iPortfolio/assets/js/common.js
+++ Site/static/iPortfolio/assets/js/common.js
@@ -1,16 +1,30 @@
 var urlParams = new URLSearchParams(window.location.search);
-function getLang()
-{return urlParams.get('lang');
+
+function getLang() {
+    return urlParams.get('lang');
 }
-function showInfo()
-{
-	//var lang="vi-VN";
-	var lang=getLang();
-	var member=members[urlParams.get('code')][lang];
-	$('.myinfo').each(function(i, obj) {		
-		$("#"+obj.id).html(member[obj.id]);
-	});	
+
+function showInfo() {
+    //var lang="vi-VN";
+    var lang = getLang();
+    var member = members[urlParams.get('code')][lang];
+    $('.myinfo').each(function(i, obj) {
+        $("#" + obj.id).html(member[obj.id]);
+    });
 }
-$( document ).ready(function() {
+
+function changeLanguage(lang) {
+    urlParams.set('lang', lang);
+    var newUrl = window.location.pathname + '?' + urlParams.toString();
+    window.history.pushState({}, '', newUrl);
     showInfo();
-});
+}
+
+$(document).ready(function() {
+    showInfo();
+
+    $('.lang').on('click', function() {
+        var lang = $(this).data('lang');
+        switchLanguage(lang);
+    });
+})
