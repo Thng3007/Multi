518545c3ea6c4c5b69ffd9d4ecf34d9c00470c1c
Thanh Nguyen CTU <thng1701@gmail.com>
2024-01-31 10:57:11 +0700

    config portfolio v3

--- Site/static/iPortfolio/assets/js/members.js
+++ Site/static/iPortfolio/assets/js/members.js
@@ -2,7 +2,7 @@ var members = {
     "2454": {
         "en": {
             "home-code": "002454",
-            "home-name": "Pham Truong Hong Ngan",
+            "home-name": " Dr. Pham Truong Hong Ngan",
             "home-slogan": "The tiger of the mountain",
             "about-intro": "About Ngan",
             "about-position": "Lecture",
@@ -12,11 +12,13 @@ var members = {
             "about-conclusion": "Officiis eligendi itaque labore et dolorum mollitia officiis optio vero. Quisquam sunt adipisci omnis et ut. Nulla accusantium dolor incidunt officia tempore. Et eius omnis. Cupiditate ut dicta maxime officiis quidem quia. Sed et consectetur qui quia repellendus itaque neque. Aliquid amet quidem ut quaerat cupiditate. Ab et eum qui repellendus omnis culpa magni laudantium dolores.",
             "about-faculty": "Faculty of Multimedia Communication - School of Infomation and Communication Technology",
             "about-phone": "0123456789",
+            "about-studied": "Resume",
+            "about-studied-p": "Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.",
+            "about-portfolio": "Portfolio",
+            "about-courses": "Courses",
+            "about-courses-p": "He has responsibility to teach some courses such as:",
 
 
-
-
-        
         },
         "vi": {
             "home-code": "002454",
@@ -29,10 +31,17 @@ var members = {
             "about-conclusion": "Officiis eligendi itaque labore et dolorum mollitia officiis optio vero. Quisquam sunt adipisci omnis et ut. Nulla accusantium dolor incidunt officia tempore. Et eius omnis. Cupiditate ut dicta maxime officiis quidem quia. Sed et consectetur qui quia repellendus itaque neque. Aliquid amet quidem ut quaerat cupiditate. Ab et eum qui repellendus omnis culpa magni laudantium dolores.",
             "about-faculty": "Khoa Truyền Thông Đa Phương Tiện - Trường Công Nghệ Thông Tin - Trường Đại Học Cần Thơ",
             "about-phone": "0123456789",
+            "about-studied": "Quá Trình Học Tập",
+            "about-studied-p": "Quá trình học abcdefghjklqeertyuiuiop",
+            "about-portfolio": "Một Số Hình Ảnh",
+            "about-courses": "Các Học Phần Giảng Dạy",
+            "about-courses-p": "Thầy đảm nhận giảng dạy một số học phần như sau:",
 
 
 
-        
+
+
+
         }
     }
 
